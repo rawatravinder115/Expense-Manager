5cc363f3d12330c83a418dc4530bc0fb29f4837f
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-24 15:40:53 +0530

    add change in ExpensesFilter dynamically

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React,{useState} from 'react';
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 // import "./App.css";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", 
+    title: "New TV", 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) 
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
   // const expense =[
@@ -11,37 +38,25 @@ function App() {
   //   {title:'car Insurance',amount:320,Date: new Date(2021,2,28)},
   //   {title:'car Insurance',amount:320,Date: new Date(2021,2,28)},
   // ];
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", 
-      title: "New TV", 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  
+
+  // just because you change some variable React wont update the component 
+// instead to achieve that you need to use state 
+
+const [expenses,setExpenses]=useState(DUMMY_EXPENSES);
 
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
-  }
+    // console.log('In App.js');
+    // console.log(expense);
+    // **************************************************
+
+    // setExpenses([expense, ...expenses]);
+
+    setExpenses((prevExpenses) =>{
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,37 @@
-import React,{useState} from 'react';
-import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
-import ExpensesFilter from './ExpensesFilter';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const[filteredYear, setFilteredYear] = useState('2020');
-
-  const filterChangeHandler = selectedYear =>{
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected ={filteredYear} onChangeFilter={filterChangeHandler} ></ExpensesFilter>
-      <ExpenseItem
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpensesFilter>
+
+       {/* two display the enpense array dynamically  */}
+
+       {/* ** you should always add such a key when mapping out lists of items  */}
+
+      {props.items.map((expense) => (
+        <ExpenseItem
+         key ={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -34,7 +50,7 @@ function Expenses(props) {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 }
